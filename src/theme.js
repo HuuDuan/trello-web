33153e0adcd866be7b82704effcf96f1b1b9ae4c
HuuDuan <duannguyenhuu04@gmail.com>
2025-09-17 16:12:00 +0700

    Refactor theme color

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,4 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles';
-import { cyan, deepOrange, orange, teal } from '@mui/material/colors';
 
 // Create a theme instance.
 const theme = extendTheme({
@@ -8,18 +7,8 @@ const theme = extendTheme({
     boardBarHeight: '60px',
   },
  colorSchemes: {
-  light: {
-    palette: {
-      primary: teal,
-      secondary: deepOrange    
-    }, 
-  },
-  dark: {
-    palette: {
-      primary: cyan,
-      secondary: orange
-    }
-  }
+  light: {},
+  dark: {}
  },
   components: {
     MuiCssBaseline: {
@@ -30,12 +19,12 @@ const theme = extendTheme({
             height: '8px'
           },
           '*::-webkit-scrollbar-thumb': {
-            backgroundColor: '#bdc3c7',
+            backgroundColor: '#dcdde1',
             borderRadius: '4px',
             
           },
           '*::-webkit-scrollbar-thumb:hover': {
-            backgroundColor: '#9da1a0ff',
+            backgroundColor: 'white',
           }
         }
       }
@@ -44,34 +33,26 @@ const theme = extendTheme({
       styleOverrides: {
         root: {
           textTransform: 'none',
+          borderWidth: '0.5px',
+          '&:hover': {
+            borderWidth: '1px',
+          }
         }
       }
     },
     MuiInputLabel: {
       styleOverrides: {
-        root: ({theme}) => ({
-          color: theme.palette.primary.main,
-          fontSize: '0.75rem',
-        })
+        root: {fontSize: '0.875rem'}
       }
     },
     MuiOutlinedInput: {
       styleOverrides: {
-        root: ({theme}) => ({
-          color: theme.palette.primary.main,
-          fontSize: '0.75rem',
-          '.MuiOutlinedInput-notchedOutline': {
-            borderColor: theme.palette.primary.light,
-          },
-          '&:hover': {
-            '.MuiOutlinedInput-notchedOutline': {
-            borderColor: theme.palette.primary.light,
-            }
-          },
-          '& fieldset': {
-            borderWidth: '1px !important',
-          }
-        })
+        root: {
+          fontSize: '0.875rem',
+          '& fieldset': {borderWidth: '0.5px !important',},
+          '&:hover fieldset': {borderWidth: '1px !important',},
+          '&.Mui-focused fieldset': {borderWidth: '1px !important',}
+        }
       },
     },
   },
