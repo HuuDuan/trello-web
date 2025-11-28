99e4c31d4ab0d105119195eeea1a9b2ecf3931d4
HuuDuan <duannguyenhuu04@gmail.com>
2025-11-28 15:34:51 +0700

    Axios call API

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,14 +1,25 @@
+import { useState, useEffect } from "react";
 import Container from "@mui/material/Container";
 import AppBar from "~/components/AppBar/AppBar";
 import BoardBar from "~/pages/Boards/BoardBar/BoardBar";
 import BoardContent from "~/pages/Boards/BoardContent/BoardContent";
 import { mockData } from "~/apis/mock-data";
+import { fetchBoardDetailsAPI } from "~/apis";
 function Board() {
+  const [board, setBoard] = useState(null);
+
+  useEffect(() => {
+    const boardId = "6924853e8712c77769c1c1cd";
+
+    fetchBoardDetailsAPI(boardId).then((board) => {
+      setBoard(board);
+    });
+  }, []);
   return (
     <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   );
 }
