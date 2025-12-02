17fd17d31b9495573a91eb56f2eb9e76eaf4e8a8
HuuDuan <duannguyenhuu04@gmail.com>
2025-12-03 01:11:48 +0700

    mongodb aggregate

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,13 +3,13 @@ import Container from "@mui/material/Container";
 import AppBar from "~/components/AppBar/AppBar";
 import BoardBar from "~/pages/Boards/BoardBar/BoardBar";
 import BoardContent from "~/pages/Boards/BoardContent/BoardContent";
-import { mockData } from "~/apis/mock-data";
+// import { mockData } from "~/apis/mock-data";
 import { fetchBoardDetailsAPI } from "~/apis";
 function Board() {
   const [board, setBoard] = useState(null);
 
   useEffect(() => {
-    const boardId = "6924853e8712c77769c1c1cd";
+    const boardId = "692972dad88eba3433fcd261";
 
     fetchBoardDetailsAPI(boardId).then((board) => {
       setBoard(board);
