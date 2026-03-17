221410815f540e851c7764d91c3eb7bf39132cef
HuuDuan <duannguyenhuu04@gmail.com>
2026-03-17 16:28:00 +0700

    lesson_4

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { activeBoardReducer } from "./activeBoard/activeBoardSlice";
 import { userReducer } from "./user/userSlice";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// cấu hình persist 
+const rootPersistConfig = {
+  key: 'root', // key của cái persist do chúng ta chỉ định, cứ để mặc định là root
+  storage: storage, // Biến storage ở trên -lưu vào localStorage
+  whitelist: ['user'] // định nghĩa các slice dữ liệu được phép duy trì qua mỗi lần f5 trình duyệt
+  // blacklist: ['user'] // định nghĩa các slice dữ liệu không được phép duy trì qua mỗi lần f5 trình duyệt
+}
+
+// Combine các reducers trong dự án chúng ta ở đây
+const reducers = combineReducers({
+  activeBoard: activeBoardReducer,
+  user: userReducer
+})
+
+// Thực hiện persist reducer
+const persistedReducers = persistReducer(rootPersistConfig, reducers)
 
 export const store = configureStore({
-  reducer: {
-    activeBoard: activeBoardReducer,
-    user: userReducer
-  }
+  reducer: persistedReducers,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false, // tắt cảnh báo về non-serializable value trong redux-persist
+  }),
 });
